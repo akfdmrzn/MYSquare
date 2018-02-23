592b11246b16c1360d76c0069f4e71955808b374
akif demirezen <demirezen@akifs-Mac.local>
2018-02-23 20:55:40 +0300

    Initial Commit

---
+++ MYSquare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MYSquare
+//
+//  Created by akif demirezen on 23/02/2018.
+//  Copyright © 2018 demirezenOrganization. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
